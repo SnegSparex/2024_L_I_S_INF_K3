1ef4b68239e82faea28553d1780e9ec364318f79
SnegSparex <snegsparex@gmail.com>
2024-04-24 20:19:48 +0200

    poprawkaviews

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Stanislav"
+moje_imie = "Stas"
 msg = "Hello World!"
 
 
